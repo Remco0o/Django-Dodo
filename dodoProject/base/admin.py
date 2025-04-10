d1e852c890bdcc0a32b3b7bccf4d8538874d2e02
Haris205 <1101746@hr.nl>
2025-04-10 09:44:43 +0200

    Created models

--- dodoProject/base/admin.py
+++ dodoProject/base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Dodo, Update, Profile
 
-# Register your models here.
+admin.site.register(Dodo)
+admin.site.register(Update)
+admin.site.register(Profile)
--- dodoProject/base/models.py
+++ dodoProject/base/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Dodo(models.Model):
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    alive = models.BooleanField(default=False)
+    dead_approved = models.BooleanField(default=False)
+    dead_approved_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                         related_name='approved_by', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Update(models.Model):
+    dodo = models.ForeignKey(Dodo, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(blank=False,)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.user
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    grade = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
