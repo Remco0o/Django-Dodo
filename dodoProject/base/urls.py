8e2c69987f9fe7cf24908655a87f4eb3a3188a16
Remco0o <remcodb20@gmail.com>
2025-04-10 09:47:20 +0200

    added login system

--- dodoProject/base/urls.py
+++ dodoProject/base/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("", include('django.contrib.auth.urls')),
+    path("register/", views.register, name="register"),
+    path("logout_user/", views.logout_user, name="logout_user"),
 ]
--- dodoProject/base/views.py
+++ dodoProject/base/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, "base/index.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
